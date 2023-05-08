e1acee2deed000ca4c313909eadd3702a565c585
ReneGlezCota <reneglezcota@gmail.com>
2023-05-08 10:35:14 -0700

    dia 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { DragDropContext } from "@hello-pangea/dnd"
 import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
@@ -6,19 +7,15 @@ import TodoCreate from "./components/TodoCreate"
 import TodoFilter from "./components/TodoFilter"
 import TodoList from "./components/TodoList"
 
-// const initialStateTodos = [
-//     {
-//         id: 1,
-//         title: "Complete online Javascript bullweb Course",
-//         completed: true,
-//     },
-//     { id: 2, title: "Go to the Gym", completed: false },
-//     { id: 3, title: "10 minutos meditation", completed: true },
-//     { id: 4, title: "Pick up groceries", completed: false },
-//     { id: 5, title: "Complete todo app on Frontend Mentor", completed: false },
-// ];
-
 const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || []
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = [...list]
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+
+    return result
+}
 
 const App = () => {
     const [todos, setTodos] = useState(initialStateTodos)
@@ -71,6 +68,20 @@ const App = () => {
         setFilter(filter)
     }
 
+    const handleDragEnd = (result) => {
+        const { destination, source } = result
+        if (!destination) return
+        if (
+            source.index === destination.index &&
+            source.droppableId === destination.droppableId
+        )
+            return
+
+        setTodos((prevTasks) =>
+            reorder(prevTasks, source.index, destination.index)
+        )
+    }
+
     return (
         <div className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat transition-all duration-1000 dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] ">
             <Header />
@@ -78,11 +89,13 @@ const App = () => {
             <main className="container mx-auto mt-8 px-4 md:max-w-xl">
                 <TodoCreate createTodo={createTodo} />
 
-                <TodoList
-                    todos={filteredTodos()}
-                    removeTodo={removeTodo}
-                    updateTodo={updateTodo}
-                />
+                <DragDropContext onDragEnd={handleDragEnd}>
+                    <TodoList
+                        todos={filteredTodos()}
+                        removeTodo={removeTodo}
+                        updateTodo={updateTodo}
+                    />
+                </DragDropContext>
 
                 <TodoComputed
                     totalNoCompletedTodo={
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,31 +1,41 @@
 import IconCheck from "./icons/IconCheck"
 import IconCross from "./icons/IconCross"
+import React from "react"
 
-const TodoItem = ({ todo, removeTodo, updateTodo }) => {
-    return (
-        <article className="flex gap-4 border-b border-b-gray-400">
-            <button
-                className={`h-5 w-5 flex-none rounded-full border-2 ${
-                    todo.completed
-                        ? "flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 "
-                        : "inline-block"
-                }`}
-                onClick={() => updateTodo(todo.id)}
+const TodoItem = React.forwardRef(
+    ({ todo, removeTodo, updateTodo, ...props }, ref) => {
+        return (
+            <article
+                {...props}
+                ref={ref}
+                className="flex gap-4 border-b border-b-gray-400"
             >
-                {todo.completed && <IconCheck />}
-            </button>
-            <p
-                className={`grow text-gray-600 ${
-                    todo.completed && "line-through"
-                }`}
-            >
-                {todo.title}
-            </p>
-            <button className="flex-none" onClick={() => removeTodo(todo.id)}>
-                <IconCross />
-            </button>
-        </article>
-    )
-}
+                <button
+                    className={`h-5 w-5 flex-none rounded-full border-2 ${
+                        todo.completed
+                            ? "flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 "
+                            : "inline-block"
+                    }`}
+                    onClick={() => updateTodo(todo.id)}
+                >
+                    {todo.completed && <IconCheck />}
+                </button>
+                <p
+                    className={`grow text-gray-600 ${
+                        todo.completed && "line-through"
+                    }`}
+                >
+                    {todo.title}
+                </p>
+                <button
+                    className="flex-none"
+                    onClick={() => removeTodo(todo.id)}
+                >
+                    <IconCross />
+                </button>
+            </article>
+        )
+    }
+)
 
 export default TodoItem
